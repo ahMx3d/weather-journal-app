5692ba6aa1c336c45532dfbdf28477fb0edf696e
AhMed SaLah <appsxapp@gmail.com>
2021-01-07 21:04:56 +0200

    Mission accomplished

--- server.js
+++ server.js
@@ -1,19 +1,47 @@
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
 
-// Require Express to run server and routes
-
-// Start up an instance of app
+// Require Express, Body-Parser, CORS prerequisites to run server and routes.
+const
+    express    = require('express'),
+    bodyParser = require('body-parser'),
+    cors       = require('cors');
+    
+// Start up an instance of app.
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// Cors for cross origin allowance
+// Cors for cross origin allowance.
+app.use(cors());
 
-// Initialize the main project folder
+// Initialize the main project folder.
 app.use(express.static('website'));
 
 
-// Setup Server
+// Setup Server.
+const port = 8000;
+app.listen(port,()=>console.log(`Server running at http://localhost:${port}/`));
+
+// GET Route.
+app.get('/all', (req, res)=>res.send(projectData).status(200).end());
+
+// POST Route.
+app.post('/data',(req, res)=>{
+    // console.log(req.body);
+    const {temp,date,utxt} = req.body;
+    projectData = {temp,date,utxt}
+    // const body = req.body;
+    // projectData = {
+    //     temp: body.temp,
+    //     date: body.date,
+    //     utxt: body.utxt
+    // }
+    // projectData['temp'] = body.temp;
+    // projectData['date'] = body.date;
+    // projectData['utxt'] = body.utxt;
+    res.send(projectData).status(200).end();
+});
--- website/app.js
+++ website/app.js
@@ -1,5 +1,83 @@
 /* Global Variables */
+const
+    apiURL      = 'http://api.openweathermap.org/data/2.5/forecast?id=',
+    apiKey      = '&appid=0d699f5298ce6c667b2a83f7971fd68f&units=metric',
+    zipInp      = document.getElementById('zip'),
+    dateDiv     = document.getElementById('date'),
+    tempDiv     = document.getElementById('temp'),
+    contentDiv  = document.getElementById('content'),
+    feelingsInp = document.getElementById('feelings'),
+    generateBtn = document.getElementById('generate'),
+    handleError = error=>console.error('Error occurred:', error);
 
 // Create a new date instance dynamically with JS
-let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let date = new Date();
+    date = `${(date.getMonth())+1}.${date.getDate()}.${date.getFullYear()}`;
+
+generateBtn.addEventListener('click',(event)=>{
+    event.preventDefault();
+    // Retrieve User Inputs.
+    const
+        zipVal  = zipInp.value,
+        utxtVal = feelingsInp.value;
+    // Fetch API Output & Store the data needed in server.
+    getInfoByZip(apiURL,zipVal,apiKey).then(data=>{
+        if(data.cod != 200) return alert(data.message);
+        post('/data',{
+            temp: data.list[0].main.temp,
+            date: date,
+            utxt: utxtVal,
+        });
+    }).catch(handleError)
+});
+
+// API information getter.
+const getInfoByZip = async (url, zip, key)=>{
+    const res = await fetch(url+zip+key);
+    try {
+        return await res.json();
+    } catch (error) {
+        console.error('Error occurred:', error);
+        alert('Oops Something wrong check the console errors');
+    }
+};
+
+// POST Handler Method.
+const post = async (url='' , data={})=>{
+    const res = await fetch(url, {
+        method     : 'POST',
+        credentials: 'same-origin',
+        headers    : {
+            'Content-Type': 'application/json'
+        },
+        body       : JSON.stringify(data)
+    });
+    try {
+        await res.json().then(updateUI()).catch(handleError);
+    } catch (error) {
+        console.error('Error occurred:', error);
+        alert('Oops Something wrong check the console errors');
+    }
+};
+
+// UI Handler Method.
+const updateUI = async ()=>{
+    const req = await fetch('/all', {
+        method     : 'GET',
+        credentials: 'same-origin',
+        headers    : {
+            'Content-Type': 'application/json'
+        }
+    });
+    try {
+        // Update UI with the server stored API data.
+        await req.json().then(data=>{
+            dateDiv.textContent    = `Date: ${data.date}`;
+            tempDiv.textContent    = `Temperature: ${data.temp}`;
+            contentDiv.textContent = `Feelings: ${data.utxt}`;
+        });
+    } catch (error) {
+        console.error('Error occurred:', error);
+        alert('Oops Something wrong check the console errors');
+    }
+};
